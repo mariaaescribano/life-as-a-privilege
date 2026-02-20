e3a135845ca61e9c0989ea9a1b5ad9ec0bf25cf7
mariaaescribano <mariaaescribanoarce3@gmail.com>
2026-02-20 21:37:50 +0100

    v1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
+  // app.enableCors(); 
+
+  app.enableCors({
+    origin: ['https://lifeasaprivilege.onrender.com'], // URL de tu frontend en Render
+    credentials: true,
+  });
 
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   await app.listen(port);
